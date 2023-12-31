b30f3f83854c790e769e7663085703f539aee790
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-12-31 21:12:46 +0200

    Update

---
+++ js/longScript.js
@@ -0,0 +1,38 @@
+// Long script begins
+
+// 
+
+const myString = 'Hello Javascript';
+
+function strEven(str) {
+    const l = str.length;
+    let even = '';
+    for(i=0; i<l; i++) {
+        if (i%2===0){
+            even = even + str[i];
+        };
+    };
+    return even;
+}
+
+function strOdd(str) {
+    const l = str.length;
+    let odd = '';
+    for(i=0; i<l; i++) {
+        if (i%2!==0){
+            odd = odd + str[i];
+        };
+    };
+    return odd;
+}
+
+const evenStr = strEven(myString);
+console.log(evenStr);
+
+const oddStr = strOdd(myString);
+console.log(oddStr);
+
+
+
+
+
